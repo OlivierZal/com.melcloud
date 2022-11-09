3128a66a453d11f0672f1b960de2c165f8807c15
Olivier Zalmanski <olivier.zalmanski@me.com>
2022-11-10 00:56:25 +0100

    Added repair support

--- mixins/driver_mixin.js
+++ mixins/driver_mixin.js
@@ -1,7 +1,7 @@
 const Homey = require('homey'); // eslint-disable-line import/no-unresolved
 
 class MELCloudDriverMixin extends Homey.Driver {
-  async onPair(session) {
+  onPair(session) {
     session.setHandler('login', async (data) => this.homey.app.login(data.username, data.password));
     session.setHandler('list_devices', async () => this.discoverDevices());
   }
@@ -12,6 +12,10 @@ class MELCloudDriverMixin extends Homey.Driver {
 
   instanceError(...message) {
     this.error(...message);
+  }
+
+  onRepair(session) {
+    session.setHandler('login', async (data) => this.homey.app.login(data.username, data.password));
   }
 }
 
