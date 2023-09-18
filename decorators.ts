19a95a7e4e3fab16c1f4c904cef1529f66c73828
Olivier Zalmanski <olivier.zalmanski@me.com>
2023-09-18 22:25:17 +0200

    improve

--- decorators.ts
+++ decorators.ts
@@ -5,7 +5,7 @@ interface HasGetNameClass {
 
 export default function logName<T extends HasGetNameClass>(
   originalMethod: (...args: any[]) => void,
-  _context: unknown, // eslint-disable-line @typescript-eslint/no-unused-vars
+  _context: ClassMethodDecoratorContext, // eslint-disable-line @typescript-eslint/no-unused-vars
 ) {
   function replacementMethod(this: T, ...args: any[]) {
     originalMethod.call(this, this.getName(), '-', ...args)
