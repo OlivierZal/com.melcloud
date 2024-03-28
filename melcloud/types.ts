4eac38f1b18f57290587d53d1280aec3a4729400
Olivier Zalmanski <olivier.zalmanski@sap.com>
2024-03-28 17:04:15 +0100

    simplify

--- melcloud/types.ts
+++ melcloud/types.ts
@@ -99,7 +99,6 @@ export const effectiveFlagsAta: Record<
   VaneHorizontal: 0x100,
   VaneVertical: 0x10,
 } as const
-export type EffectiveFlagsAta = typeof effectiveFlagsAta
 export type PostDataAta = BasePostData & Readonly<SetDeviceDataAta>
 export interface DeviceDataAta extends SetDeviceDataAta {
   readonly RoomTemperature: number
@@ -157,7 +156,6 @@ export const effectiveFlagsAtw: Record<
   SetTemperatureZone1: 0x200000080,
   SetTemperatureZone2: 0x800000200,
 } as const
-export type EffectiveFlagsAtw = typeof effectiveFlagsAtw
 export type PostDataAtw = BasePostData & Readonly<SetDeviceDataAtw>
 export interface DeviceDataAtw extends SetDeviceDataAtw {
   readonly IdleZone1: boolean
@@ -217,7 +215,6 @@ export const effectiveFlagsErv: Record<
   NonEffectiveFlagsKeyOf<SetDeviceDataErv>,
   number
 > = { Power: 0x1, SetFanSpeed: 0x8, VentilationMode: 0x4 } as const
-export type EffectiveFlagsErv = typeof effectiveFlagsErv
 export type PostDataErv = BasePostData & Readonly<SetDeviceDataErv>
 export interface DeviceDataErv extends SetDeviceDataErv {
   readonly OutdoorTemperature: number
@@ -244,9 +241,9 @@ export interface SetDeviceData {
   readonly Erv: SetDeviceDataErv
 }
 export interface EffectiveFlags {
-  readonly Ata: EffectiveFlagsAta
-  readonly Atw: EffectiveFlagsAtw
-  readonly Erv: EffectiveFlagsErv
+  readonly Ata: typeof effectiveFlagsAta
+  readonly Atw: typeof effectiveFlagsAtw
+  readonly Erv: typeof effectiveFlagsErv
 }
 export interface PostData {
   readonly Ata: PostDataAta
