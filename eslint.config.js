06c205921a3ff77f2b395d2564a6dbd52470f6ca
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-04-19 23:31:33 +0200

    improve

--- eslint.config.js
+++ eslint.config.js
@@ -173,9 +173,14 @@ module.exports = tsEslint.config(
       'sort-keys': ['error', 'asc', { natural: true }],
     },
   },
+  /*
+   * {
+   *   rules: importPlugin.configs.recommended.rules,
+   * },
+   */
   {
+    files: ['**/*.ts'],
     rules: {
-      // ...importPlugin.configs.recommended.rules,
       ...importPlugin.configs.typescript.rules,
       'import/no-duplicates': ['error', { 'prefer-inline': true }],
     },
