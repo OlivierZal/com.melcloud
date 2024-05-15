092336553811f726493dfa530ef2d059ec02ce54
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-05-15 16:56:49 +0200

    improve

--- eslint.config.js
+++ eslint.config.js
@@ -291,6 +291,7 @@ module.exports = tsEslint.config(
     files: ['**/*.js'],
     languageOptions: {
       globals: globals.node,
+      sourceType: 'commonjs',
     },
     rules: {
       '@typescript-eslint/no-var-requires': 'off',
