141cada998ca4cc91d54cdd2cf2cbb2228a77acc
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-09-06 20:45:09 +0200

    better

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -367,8 +367,8 @@ export default [
     },
   ),
   {
-    ...html.configs['flat/recommended'],
     files: ['**/*.html'],
+    ...html.configs['flat/recommended'],
     rules: {
       ...html.configs['flat/recommended'].rules,
       '@html-eslint/id-naming-convention': 'error',
