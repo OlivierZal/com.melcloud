bf3e52376f1736158443adfd89b6755787642379
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-09-10 23:07:52 +0200

    better

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -156,7 +156,7 @@ export default [
         '@typescript-eslint/no-unused-vars': [
           'error',
           {
-            argsIgnorePattern: '^_',
+            argsIgnorePattern: '^_context$',
             caughtErrorsIgnorePattern: '^_',
             varsIgnorePattern: 'onHomeyReady',
           },
