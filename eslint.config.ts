90cf3ca69140190e0e446ebcabb83d68bb416055
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-11-12 00:23:27 +0100

    improve

--- eslint.config.ts
+++ eslint.config.ts
@@ -395,6 +395,12 @@ const config = [
         'import/unambiguous': 'error',
         'max-lines': 'off',
         'no-bitwise': 'off',
+        'no-else-return': [
+          'error',
+          {
+            allowElseIf: false,
+          },
+        ],
         'no-empty': [
           'error',
           {
