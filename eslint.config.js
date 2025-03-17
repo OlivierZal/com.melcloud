a3d4c53a5eadb8075397145f9e2c5b8aa7b957ee
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2025-03-17 09:42:10 +0100

    simplify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -10,10 +10,7 @@ import packageJson from 'eslint-plugin-package-json/configs/recommended'
 import perfectionist from 'eslint-plugin-perfectionist'
 import yml from 'eslint-plugin-yml'
 import { defineConfig } from 'eslint/config'
-import {
-  config as defineTSConfig,
-  configs as tsConfigs,
-} from 'typescript-eslint'
+import { configs as tsConfigs } from 'typescript-eslint'
 
 import { classGroups } from './eslint-utils/class-groups.js'
 
@@ -138,278 +135,276 @@ const config = defineConfig([
   {
     ignores: ['.homeybuild/'],
   },
-  ...defineTSConfig(
-    {
-      extends: [
-        js.configs.all,
-        tsConfigs.all,
-        tsConfigs.strictTypeChecked,
-        importPlugin.flatConfigs.errors,
-        importPlugin.flatConfigs.typescript,
-        prettier,
+  {
+    extends: [
+      js.configs.all,
+      tsConfigs.all,
+      tsConfigs.strictTypeChecked,
+      importPlugin.flatConfigs.errors,
+      importPlugin.flatConfigs.typescript,
+      prettier,
+    ],
+    files: ['**/*.{ts,mts,js}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['*.js'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+      sourceType: 'module',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    plugins: {
+      '@stylistic': stylistic,
+      perfectionist,
+    },
+    rules: {
+      '@stylistic/line-comment-position': 'error',
+      '@stylistic/lines-around-comment': 'error',
+      '@stylistic/lines-between-class-members': ['error', 'always'],
+      '@stylistic/multiline-comment-style': 'error',
+      '@stylistic/quotes': [
+        'error',
+        'single',
+        {
+          allowTemplateLiterals: false,
+          avoidEscape: true,
+          ignoreStringLiterals: false,
+        },
       ],
-      files: ['**/*.{ts,mts,js}'],
-      languageOptions: {
-        ecmaVersion: 'latest',
-        parserOptions: {
-          projectService: {
-            allowDefaultProject: ['*.js'],
-          },
-          tsconfigRootDir: import.meta.dirname,
-          warnOnUnsupportedTypeScriptVersion: false,
-        },
-        sourceType: 'module',
-      },
-      linterOptions: {
-        reportUnusedDisableDirectives: true,
-      },
-      plugins: {
-        '@stylistic': stylistic,
-        perfectionist,
-      },
-      rules: {
-        '@stylistic/line-comment-position': 'error',
-        '@stylistic/lines-around-comment': 'error',
-        '@stylistic/lines-between-class-members': ['error', 'always'],
-        '@stylistic/multiline-comment-style': 'error',
-        '@stylistic/quotes': [
-          'error',
-          'single',
-          {
-            allowTemplateLiterals: false,
-            avoidEscape: true,
-            ignoreStringLiterals: false,
-          },
-        ],
-        '@stylistic/spaced-comment': [
-          'error',
-          'always',
-          {
-            block: {
-              balanced: true,
-            },
-          },
-        ],
-        '@typescript-eslint/consistent-return': 'off',
-        '@typescript-eslint/consistent-type-assertions': [
-          'error',
-          {
-            arrayLiteralTypeAssertions: 'never',
-            assertionStyle: 'as',
-            objectLiteralTypeAssertions: 'never',
-          },
-        ],
-        '@typescript-eslint/member-ordering': 'off',
-        '@typescript-eslint/naming-convention': [
-          'error',
-          {
-            filter: {
-              match: true,
-              regex: '^EnergyReport(Regular|Total)$',
-            },
-            format: null,
-            selector: 'property',
-          },
-          {
-            filter: {
-              match: true,
-              regex: '^[a-z]+(?:_[a-z0-9]+)*(\\.(?:[a-z0-9]+_)*([a-z0-9]+)?)?$',
-            },
-            format: null,
-            selector: ['objectLiteralProperty', 'typeProperty'],
-          },
-          {
-            format: ['camelCase', 'PascalCase'],
-            selector: ['objectLiteralProperty', 'typeProperty'],
-          },
-          {
-            format: ['camelCase', 'PascalCase'],
-            selector: 'import',
-          },
-          {
-            format: ['PascalCase'],
-            prefix: ['can', 'did', 'has', 'is', 'should', 'will'],
-            selector: 'variable',
-            types: ['boolean'],
-          },
-          {
-            format: ['UPPER_CASE'],
-            modifiers: ['const', 'global'],
-            selector: 'variable',
-            types: ['boolean', 'number', 'string'],
-          },
-          {
-            format: ['PascalCase'],
-            selector: 'typeLike',
-          },
-          {
-            format: ['camelCase'],
-            leadingUnderscore: 'allow',
-            selector: 'default',
-          },
-        ],
-        '@typescript-eslint/no-dupe-class-members': 'off',
-        '@typescript-eslint/no-explicit-any': [
-          'error',
-          {
-            ignoreRestArgs: true,
-          },
-        ],
-        '@typescript-eslint/no-invalid-this': 'off',
-        '@typescript-eslint/no-redeclare': 'off',
-        '@typescript-eslint/no-unnecessary-condition': [
-          'error',
-          {
-            checkTypePredicates: true,
-          },
-        ],
-        '@typescript-eslint/no-unsafe-type-assertion': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          {
-            argsIgnorePattern: '^_context$',
-            varsIgnorePattern: '^onHomeyReady$',
-          },
-        ],
-        '@typescript-eslint/prefer-destructuring': [
-          'error',
-          {
-            array: true,
-            object: true,
-          },
-          {
-            enforceForDeclarationWithTypeAnnotation: true,
-            enforceForRenamedProperties: true,
-          },
-        ],
-        '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-        '@typescript-eslint/return-await': ['error', 'in-try-catch'],
-        '@typescript-eslint/typedef': 'off',
-        camelcase: 'off',
-        curly: 'error',
-        'import/first': 'error',
-        'import/max-dependencies': [
-          'error',
-          {
-            ignoreTypeImports: true,
-          },
-        ],
-        'import/newline-after-import': 'error',
-        'import/no-absolute-path': 'error',
-        'import/no-anonymous-default-export': 'error',
-        'import/no-cycle': 'error',
-        'import/no-default-export': 'error',
-        'import/no-deprecated': 'error',
-        'import/no-duplicates': 'error',
-        'import/no-dynamic-require': 'error',
-        'import/no-empty-named-blocks': 'error',
-        'import/no-extraneous-dependencies': 'error',
-        'import/no-import-module-exports': 'error',
-        'import/no-mutable-exports': 'error',
-        'import/no-named-as-default': 'error',
-        'import/no-named-as-default-member': 'error',
-        'import/no-named-default': 'error',
-        'import/no-namespace': 'error',
-        'import/no-relative-packages': 'error',
-        'import/no-self-import': 'error',
-        'import/no-unassigned-import': [
-          'error',
-          {
-            allow: ['source-map-support/register.js', 'core-js/actual/**'],
-          },
-        ],
-        'import/no-unused-modules': 'error',
-        'import/no-useless-path-segments': 'error',
-        'import/no-webpack-loader-syntax': 'error',
-        'import/unambiguous': 'error',
-        'max-lines': 'off',
-        'no-bitwise': 'off',
-        'no-else-return': [
-          'error',
-          {
-            allowElseIf: false,
-          },
-        ],
-        'no-empty': [
-          'error',
-          {
-            allowEmptyCatch: true,
-          },
-        ],
-        'no-ternary': 'off',
-        'no-undefined': 'off',
-        'no-underscore-dangle': [
-          'error',
-          {
-            allow: ['__'],
-          },
-        ],
-        'one-var': ['error', 'never'],
-        'perfectionist/sort-array-includes': ['error', arrayLikeSortOptions],
-        'perfectionist/sort-classes': ['error', classSortOptions],
-        'perfectionist/sort-decorators': ['error', decoratorSortOptions],
-        'perfectionist/sort-enums': ['error', enumSortOptions],
-        'perfectionist/sort-exports': ['error', exportGroupKind],
-        'perfectionist/sort-heritage-clauses': 'error',
-        'perfectionist/sort-imports': ['error', importSortOptions],
-        'perfectionist/sort-interfaces': ['error', typeLikeSortOptions],
-        'perfectionist/sort-intersection-types': ['error', typeGroups],
-        'perfectionist/sort-maps': ['error', mapSortOptions],
-        'perfectionist/sort-modules': ['error', moduleSortOptions],
-        'perfectionist/sort-named-exports': ['error', namedSortOptions],
-        'perfectionist/sort-named-imports': ['error', namedSortOptions],
-        'perfectionist/sort-object-types': ['error', typeLikeSortOptions],
-        'perfectionist/sort-objects': ['error', objectSortOptions],
-        'perfectionist/sort-sets': ['error', arrayLikeSortOptions],
-        'perfectionist/sort-switch-case': 'error',
-        'perfectionist/sort-union-types': ['error', typeGroups],
-        'sort-imports': 'off',
-        'sort-keys': 'off',
-      },
-      settings: {
-        perfectionist: {
-          ignoreCase: false,
-          locales: 'en_US',
-          order: 'asc',
-          partitionByComment: true,
-          partitionByNewLine: false,
-          type: 'natural',
-        },
-        ...importPlugin.flatConfigs.typescript.settings,
-        'import/resolver': {
-          ...importPlugin.flatConfigs.typescript.settings['import/resolver'],
-          typescript: {
-            alwaysTryTypes: true,
+      '@stylistic/spaced-comment': [
+        'error',
+        'always',
+        {
+          block: {
+            balanced: true,
           },
         },
-      },
-    },
-    {
-      extends: [tsConfigs.disableTypeChecked],
-      files: ['**/*.js'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-      },
-    },
-    {
-      files: [
-        '**/*.config.{ts,js}',
-        '**/api.mts',
-        'app.mts',
-        'drivers/*/{device,driver}.mts',
       ],
-      rules: {
-        'import/no-default-export': 'off',
-        'import/prefer-default-export': [
-          'error',
-          {
-            target: 'any',
+      '@typescript-eslint/consistent-return': 'off',
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        {
+          arrayLiteralTypeAssertions: 'never',
+          assertionStyle: 'as',
+          objectLiteralTypeAssertions: 'never',
+        },
+      ],
+      '@typescript-eslint/member-ordering': 'off',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          filter: {
+            match: true,
+            regex: '^EnergyReport(Regular|Total)$',
           },
-        ],
+          format: null,
+          selector: 'property',
+        },
+        {
+          filter: {
+            match: true,
+            regex: '^[a-z]+(?:_[a-z0-9]+)*(\\.(?:[a-z0-9]+_)*([a-z0-9]+)?)?$',
+          },
+          format: null,
+          selector: ['objectLiteralProperty', 'typeProperty'],
+        },
+        {
+          format: ['camelCase', 'PascalCase'],
+          selector: ['objectLiteralProperty', 'typeProperty'],
+        },
+        {
+          format: ['camelCase', 'PascalCase'],
+          selector: 'import',
+        },
+        {
+          format: ['PascalCase'],
+          prefix: ['can', 'did', 'has', 'is', 'should', 'will'],
+          selector: 'variable',
+          types: ['boolean'],
+        },
+        {
+          format: ['UPPER_CASE'],
+          modifiers: ['const', 'global'],
+          selector: 'variable',
+          types: ['boolean', 'number', 'string'],
+        },
+        {
+          format: ['PascalCase'],
+          selector: 'typeLike',
+        },
+        {
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+          selector: 'default',
+        },
+      ],
+      '@typescript-eslint/no-dupe-class-members': 'off',
+      '@typescript-eslint/no-explicit-any': [
+        'error',
+        {
+          ignoreRestArgs: true,
+        },
+      ],
+      '@typescript-eslint/no-invalid-this': 'off',
+      '@typescript-eslint/no-redeclare': 'off',
+      '@typescript-eslint/no-unnecessary-condition': [
+        'error',
+        {
+          checkTypePredicates: true,
+        },
+      ],
+      '@typescript-eslint/no-unsafe-type-assertion': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_context$',
+          varsIgnorePattern: '^onHomeyReady$',
+        },
+      ],
+      '@typescript-eslint/prefer-destructuring': [
+        'error',
+        {
+          array: true,
+          object: true,
+        },
+        {
+          enforceForDeclarationWithTypeAnnotation: true,
+          enforceForRenamedProperties: true,
+        },
+      ],
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      '@typescript-eslint/return-await': ['error', 'in-try-catch'],
+      '@typescript-eslint/typedef': 'off',
+      camelcase: 'off',
+      curly: 'error',
+      'import/first': 'error',
+      'import/max-dependencies': [
+        'error',
+        {
+          ignoreTypeImports: true,
+        },
+      ],
+      'import/newline-after-import': 'error',
+      'import/no-absolute-path': 'error',
+      'import/no-anonymous-default-export': 'error',
+      'import/no-cycle': 'error',
+      'import/no-default-export': 'error',
+      'import/no-deprecated': 'error',
+      'import/no-duplicates': 'error',
+      'import/no-dynamic-require': 'error',
+      'import/no-empty-named-blocks': 'error',
+      'import/no-extraneous-dependencies': 'error',
+      'import/no-import-module-exports': 'error',
+      'import/no-mutable-exports': 'error',
+      'import/no-named-as-default': 'error',
+      'import/no-named-as-default-member': 'error',
+      'import/no-named-default': 'error',
+      'import/no-namespace': 'error',
+      'import/no-relative-packages': 'error',
+      'import/no-self-import': 'error',
+      'import/no-unassigned-import': [
+        'error',
+        {
+          allow: ['source-map-support/register.js', 'core-js/actual/**'],
+        },
+      ],
+      'import/no-unused-modules': 'error',
+      'import/no-useless-path-segments': 'error',
+      'import/no-webpack-loader-syntax': 'error',
+      'import/unambiguous': 'error',
+      'max-lines': 'off',
+      'no-bitwise': 'off',
+      'no-else-return': [
+        'error',
+        {
+          allowElseIf: false,
+        },
+      ],
+      'no-empty': [
+        'error',
+        {
+          allowEmptyCatch: true,
+        },
+      ],
+      'no-ternary': 'off',
+      'no-undefined': 'off',
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__'],
+        },
+      ],
+      'one-var': ['error', 'never'],
+      'perfectionist/sort-array-includes': ['error', arrayLikeSortOptions],
+      'perfectionist/sort-classes': ['error', classSortOptions],
+      'perfectionist/sort-decorators': ['error', decoratorSortOptions],
+      'perfectionist/sort-enums': ['error', enumSortOptions],
+      'perfectionist/sort-exports': ['error', exportGroupKind],
+      'perfectionist/sort-heritage-clauses': 'error',
+      'perfectionist/sort-imports': ['error', importSortOptions],
+      'perfectionist/sort-interfaces': ['error', typeLikeSortOptions],
+      'perfectionist/sort-intersection-types': ['error', typeGroups],
+      'perfectionist/sort-maps': ['error', mapSortOptions],
+      'perfectionist/sort-modules': ['error', moduleSortOptions],
+      'perfectionist/sort-named-exports': ['error', namedSortOptions],
+      'perfectionist/sort-named-imports': ['error', namedSortOptions],
+      'perfectionist/sort-object-types': ['error', typeLikeSortOptions],
+      'perfectionist/sort-objects': ['error', objectSortOptions],
+      'perfectionist/sort-sets': ['error', arrayLikeSortOptions],
+      'perfectionist/sort-switch-case': 'error',
+      'perfectionist/sort-union-types': ['error', typeGroups],
+      'sort-imports': 'off',
+      'sort-keys': 'off',
+    },
+    settings: {
+      perfectionist: {
+        ignoreCase: false,
+        locales: 'en_US',
+        order: 'asc',
+        partitionByComment: true,
+        partitionByNewLine: false,
+        type: 'natural',
+      },
+      ...importPlugin.flatConfigs.typescript.settings,
+      'import/resolver': {
+        ...importPlugin.flatConfigs.typescript.settings['import/resolver'],
+        typescript: {
+          alwaysTryTypes: true,
+        },
       },
     },
-  ),
+  },
+  {
+    extends: [tsConfigs.disableTypeChecked],
+    files: ['**/*.js'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  {
+    files: [
+      '**/*.config.{ts,js}',
+      '**/api.mts',
+      'app.mts',
+      'drivers/*/{device,driver}.mts',
+    ],
+    rules: {
+      'import/no-default-export': 'off',
+      'import/prefer-default-export': [
+        'error',
+        {
+          target: 'any',
+        },
+      ],
+    },
+  },
   {
     extends: [html.configs['flat/recommended']],
     files: ['**/*.html'],
